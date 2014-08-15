4faf9e8c418bc5d4610e4480779d270267818ed0
Artem <namit@yandex.ru>
2014-08-15 10:14:33 +0400

    Update schema-manipulator.js
    removed todo

--- schema-manipulator.js
+++ schema-manipulator.js
@@ -1,6 +1,5 @@
-(function(global){
+(function (global) {
 	"use strict";
-	//@TODO: сделать кастомное дом событие changeObj, на которое будет подписываться юзер, которое будет эмититься, когда корневой обьект изменён
 
 	function closest(elem, selector) {
 		while (elem) {
@@ -12,60 +11,61 @@
 		}
 		return false;
 	}
-	function SchemaItem(schema){
+
+	function SchemaItem(schema) {
 		_.merge(this, schema);
-		if(schema.type == 'object'){
-			for(var key in schema.properties) {
+		if (schema.type == 'object') {
+			for (var key in schema.properties) {
 				this.properties[key] = new SchemaItem(schema.properties[key]);
 			}
-		} else if(schema.type == 'array') {
+		} else if (schema.type == 'array') {
 			this.items = new SchemaItem(this.items);
 
-		} else if(schema.type == 'string' || schema.type == 'integer' || schema.type == 'number'){
+		} else if (schema.type == 'string' || schema.type == 'integer' || schema.type == 'number') {
 
 		}
 	}
 
-	function makeEl(tagname, attributes, children){
+	function makeEl(tagname, attributes, children) {
 		var el = document.createElement(tagname);
-		_.forEach(attributes, function(val, key){
+		_.forEach(attributes, function (val, key) {
 			el.setAttribute(key, val);
 		});
-		if(children){
-			if(typeof children == 'string'){
+		if (children) {
+			if (typeof children == 'string') {
 				el.innerHTML = children;
-			} else if(children instanceof Array){
-				children.forEach(function(item){
+			} else if (children instanceof Array) {
+				children.forEach(function (item) {
 					el.appendChild(item);
 				});
-			}else{
+			} else {
 				el.appendChild(children);
 			}
 		}
 		return el;
 	}
 
-	function makeInput(obj, schema, wrapper, name, namePrefix){
+	function makeInput(obj, schema, wrapper, name, namePrefix) {
 		var obj = obj instanceof Object ? '' : obj;
 		wrapper.classList.add('form-group');
 		wrapper.classList.add('input-' + name);
 		var attributes = schema.attributes || {};
-		if(schema.type == 'string' || schema.type == 'number' || schema.type == 'any'){
-			if(schema.widget){
-				if(schema.widget == 'textarea'){
+		if (schema.type == 'string' || schema.type == 'number' || schema.type == 'any') {
+			if (schema.widget) {
+				if (schema.widget == 'textarea') {
 					var el = makeEl('textarea');
 
-				} else if(schema.widget == 'select'){
+				} else if (schema.widget == 'select') {
 					var el = makeEl('select');
 					el.appendChild(makeEl('option'));
-					_.forEach(schema.options, function(val, key){
+					_.forEach(schema.options, function (val, key) {
 						var option = makeEl('option');
 						option.setAttribute('value', key);
 						option.innerHTML = val;
 						el.appendChild(option);
 					});
 
-				} else if(schema.widget == 'file'){
+				} else if (schema.widget == 'file') {
 					var el = makeEl('input', {
 						type: 'file'
 					});
@@ -83,24 +83,24 @@
 
 			}
 
-		} else if(schema.type == 'integer') {
+		} else if (schema.type == 'integer') {
 			var el = makeEl('input', {
 				type: 'number'
 			});
 		}
 
 		attributes.name = name ? (namePrefix ? [namePrefix, name].join('.') : name) : '';
-		_.forEach(attributes, function(val, key){
+		_.forEach(attributes, function (val, key) {
 			el.setAttribute(key, val);
 		});
 		el.classList.add('form-control');
-		if(schema.widget){
+		if (schema.widget) {
 			el.classList.add('widget-' + schema.widget);
 		}
 		el.value = obj;
-		el.addEventListener('change', function(){
+		el.addEventListener('change', function () {
 			var parent;
-			if(this.parentNode.parentNode.classList.contains('object')){
+			if (this.parentNode.parentNode.classList.contains('object')) {
 				parent = this.parentNode.parentNode;
 			} else {
 				parent = this.parentNode.parentNode.parentNode;
@@ -112,7 +112,7 @@
 		wrapper.appendChild(el);
 	}
 
-	function makeRow(objPart, schema, wrapper, i, namePrefix){
+	function makeRow(objPart, schema, wrapper, i, namePrefix) {
 		var row = schema.items.form(objPart, i, namePrefix);
 		var btn = makeEl('button', {
 			'class': 'btn remove'
@@ -124,43 +124,43 @@
 				'class': 'text'
 			}, 'Remove item')
 		]);
-		btn.addEventListener('click', function(){
+		btn.addEventListener('click', function () {
 			this.parentNode.parentNode.removeChild(this.parentNode);
 		}, false);
 		row.appendChild(btn);
 		wrapper.appendChild(row);
 	}
 
-	SchemaItem.prototype.form = function(obj, name, namePrefix) {
+	SchemaItem.prototype.form = function (obj, name, namePrefix) {
 		var schema = this;
-		if(schema.type == 'array' && !obj instanceof Array){
+		if (schema.type == 'array' && !obj instanceof Array) {
 			obj = [];
 		}
 		var wrapper = makeEl('div');
 		wrapper.obj = obj;
-		if(schema.label) {
+		if (schema.label) {
 			wrapper.appendChild(makeEl('label', {}, schema.label));
 		}
-		if(!name){
+		if (!name) {
 			wrapper.classList.add('object-root');
 			var event = new CustomEvent('changeObj', { detail: wrapper.obj });
-			wrapper.changeObj = function(){
+			wrapper.changeObj = function () {
 				this.dispatchEvent(event);
 			};
 		}
-		if(schema.type == 'array') {
+		if (schema.type == 'array') {
 			wrapper.classList.add('array');
 			wrapper.itemsCount = 0;
 			wrapper.schema = schema;
-			if(name) {
+			if (name) {
 				wrapper.classList.add('array-' + name);
 			}
 
 			var items = makeEl('div');
 			items.classList.add('items');
-			if(obj instanceof Array) {
+			if (obj instanceof Array) {
 				wrapper.itemsCount = obj.length;
-				obj.forEach(function(objPart, key) {
+				obj.forEach(function (objPart, key) {
 					makeRow(objPart, schema, items, key.toString(), name);
 				});
 			}
@@ -177,7 +177,7 @@
 				}, 'Add item')
 			]);
 			wrapper.appendChild(btn);
-			btn.addEventListener("click", function() {
+			btn.addEventListener("click", function () {
 				var items = this.previousSibling;
 				var newObj = {};
 				this.parentNode.obj.push(newObj);
@@ -185,7 +185,7 @@
 				wrapper.itemsCount++;
 			}, false);
 
-		} else if(schema.type == 'object'){
+		} else if (schema.type == 'object') {
 			wrapper.classList.add('object');
 			if (name) {
 				wrapper.classList.add('object-' + name);
@@ -193,16 +193,16 @@
 			if (namePrefix) {
 				name = [namePrefix, name].join('.');
 			}
-			_.forEach(schema.properties, function(schemaPart, key){
-				if(!obj.hasOwnProperty(key)){
+			_.forEach(schema.properties, function (schemaPart, key) {
+				if (!obj.hasOwnProperty(key)) {
 					var objPart;
-					if(schemaPart.type == 'array'){
+					if (schemaPart.type == 'array') {
 						objPart = [];
-					} else if(schemaPart.type == 'object'){
+					} else if (schemaPart.type == 'object') {
 						objPart = {};
-					}else if(schemaPart.type == 'string'){
+					} else if (schemaPart.type == 'string') {
 						objPart = '';
-					} else if(schemaPart.type == 'number'){
+					} else if (schemaPart.type == 'number') {
 						objPart = 0;
 					}
 					obj[key] = objPart;
@@ -212,7 +212,7 @@
 				wrapper.appendChild(schemaPart.form(objPart, key, name));
 			});
 
-		} else if(schema.type == 'string' || schema.type == 'integer' || schema.type == 'number' || schema.type == 'any'){
+		} else if (schema.type == 'string' || schema.type == 'integer' || schema.type == 'number' || schema.type == 'any') {
 			makeInput(obj, schema, wrapper, name, namePrefix);
 		} else {
 			wrapper.classList.add('alert');
@@ -223,12 +223,12 @@
 	};
 
 	var defineAsGlobal = true;
-	if(typeof exports === 'object') {
-	    module.exports = SchemaItem;
-	    defineAsGlobal = false;
+	if (typeof exports === 'object') {
+		module.exports = SchemaItem;
+		defineAsGlobal = false;
 	}
-	if(typeof define === 'function') {
-		define(function(require, exports, module) {
+	if (typeof define === 'function') {
+		define(function (require, exports, module) {
 			module.exports = SchemaItem;
 		});
 		defineAsGlobal = false;
